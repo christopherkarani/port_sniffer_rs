a01ae7266f6e7ec34a60eea2a712cc2f3f94d3e9
johnnyMad-Dog <chrisbkarani@gmail.com>
2022-05-20 12:26:33 +0000

    added arguments

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::net::IpAddr;
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let progam = args[0].clone();
+}
